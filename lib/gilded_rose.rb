aa8b8a2680f656fd73a316b7a630e9b42a4a322c
Praveen Varadharaj <pvrdhraj@memphis.edu>
2024-02-27 21:27:03 -0600

    Finished the complete refactor of Gilded Rose.

--- lib/gilded_rose.rb
+++ lib/gilded_rose.rb
@@ -1,39 +1,16 @@
-class GildedRose
-  attr_reader :item
-
-  def initialize(name, quality, days_remaining)
-    @item = klass_for(name).new(quality, days_remaining)
-  
-  def klass_for(name)
-    case name
-    when 'normal'
-      Normal
-    when 'Aged Brie'
-      Brie
-    when 'Sulfuras, Hand of Ragnaros'
-      Sulfuras
-    when 'Backstage passes to a TAFKAL80ETC concert'
-      Backstage
-    end
-  end
-
-  def tick
-    item.tick
-  end
-
-  def quality
-    item.quality
-  end
-
-  def days_remaining
-    item.days_remaining
-  end
-
-  class Normal
+module GildedRose
+  class Item
     attr_reader :quality, :days_remaining
+
     def initialize(quality, days_remaining)
       @quality, @days_remaining = quality, days_remaining
     end
+
+    def tick
+    end
+  end
+
+  class Normal < Item
     def tick
       @days_remaining -= 1
       return if @quality == 0
@@ -43,11 +20,7 @@ class GildedRose
     end
   end
 
-  class Brie
-    attr_reader :quality, :days_remaining
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
+  class Brie < Item
     def tick
       @days_remaining -= 1
       return if @quality >= 50
@@ -57,21 +30,8 @@ class GildedRose
     end
   end
 
-  class Sulfuras
-    attr_reader :quality, :days_remaining
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
+  class Backstage < Item
     def tick
-    end
-  end
-
-  class Backstage
-    attr_reader :quality, :days_remaining
-    def initialize(quality, days_remaining)
-      @quality, @days_remaining = quality, days_remaining
-    end
-    def backstage_tick
       @days_remaining -= 1
       return              if @quality >= 50
       return @quality = 0 if @days_remaining < 0
@@ -80,6 +40,27 @@ class GildedRose
       @quality += 1 if @days_remaining < 10
       @quality += 1 if @days_remaining < 5
     end
+  end
+
+  class Conjured < Item
+    def tick
+      @days_remaining -= 1
+      return if @quality == 0
+
+      @quality -= 2
+      @quality -= 2 if @days_remaining <= 0
+    end
+  end
+
+  DEFAULT_CLASS = Item
+  SPECIALIZED_CLASSES = {
+    'normal'                                          => Normal,
+    'Aged Brie'                                       => Brie,
+    'Backstage passes to a TAFKAL80ETC concert'       => Backstage,
+    'Conjured Mana Cake'                              => Conjured  }
+  
+  def self.for(name, quality, days_remaining)
+    (SPECIALIZED_CLASSES[name] || DEFAULT_CLASS).new(quality, days_remaining)
   end
 end
 
